bfcc095aef9182a5fa0f9b84a67542d2c6c3736f
Md Nazid <mdnajid266@gmail.com>
2025-10-30 17:08:19 +0600

    Fix f-string syntax error in humanized_date filter

--- event_users/templatetags/customs_filters.py
+++ event_users/templatetags/customs_filters.py
@@ -11,10 +11,10 @@ def humanized_date(valu):
         valu=timezone.localtime(valu)
         
         if valu.date()==today:
-            return f'Today At {valu.strftime('%I:%M %p')}'
+            return f"Today At {valu.strftime('%I:%M %p')}"
         if valu.date()==today.replace(day=today.day-1):
-            return f'Yesterday At {valu.strftime('%I:%M %p')}'
+            return f"Yesterday At {valu.strftime('%I:%M %p')}"
         else:
-            return f'{valu.date().strftime('%B %d'),valu.strftime('%I:%M %p')}'
+            return f"{valu.date().strftime('%B %d'),valu.strftime('%I:%M %p')}"
     return "No Login Record Available"
             
